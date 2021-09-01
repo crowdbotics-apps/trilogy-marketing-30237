6498db13cdbf103c6d11a56691448ac5a722f811
Crowdbotics <team@crowdbotics.com>
2021-09-01 00:48:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "trilogy_marketing_30237Identifier",
+  url: "https://trilogy_marketing_30237Identifier.botics.co",
+  api: "https://trilogy_marketing_30237Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
